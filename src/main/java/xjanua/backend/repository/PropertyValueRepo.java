64cb0674138234468d400bf283f3b6e0afb867a4
tienchinh21 <tienchinh21.it@gmail.com>
2026-03-31 23:03:24 +0700

    remove file unuse

--- src/main/java/xjanua/backend/repository/PropertyValueRepo.java
+++
@@ -1,8 +0,0 @@
-package xjanua.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import xjanua.backend.model.PropertyValue;
-
-public interface PropertyValueRepo extends JpaRepository<PropertyValue, String> {
-}
