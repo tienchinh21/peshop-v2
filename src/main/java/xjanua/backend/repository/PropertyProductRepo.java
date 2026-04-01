4a68688701e060cc20374df3a030dfa6fa60a907
tandattt <nguyentandat18122004@gmail.com>
2026-04-01 14:35:26 +0700

    feat: CRUD product

---
+++ src/main/java/xjanua/backend/repository/PropertyProductRepo.java
@@ -0,0 +1,9 @@
+package xjanua.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import xjanua.backend.model.PropertyProduct;
+
+public interface PropertyProductRepo extends JpaRepository<PropertyProduct, String> {
+
+}
